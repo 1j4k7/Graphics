791b36ce5b5ab4879a51558568770167f01c29a0
1j4k7 <jasonkuo147@gmail.com>
2015-09-18 16:47:05 -0400

    SPACE

--- PursuitCurve/src/AdvancedTriangle.java
+++ PursuitCurve/src/AdvancedTriangle.java
@@ -3,7 +3,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
