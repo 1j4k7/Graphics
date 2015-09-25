d5a1797c779aa7485e9f32d84d9c2bcf6a1c95c6
1j4k7 <jasonkuo147@gmail.com>
2015-09-24 22:48:56 -0400

    added tesselation to pursuit curves

--- PursuitCurve/src/AdvancedTriangle.java
+++ PursuitCurve/src/AdvancedTriangle.java
@@ -22,7 +22,7 @@ public class AdvancedTriangle extends JFrame {
 	public static void main(String[] args) {
 		AdvancedTriangle frame = new AdvancedTriangle();//create a frame
 		frame.setTitle("Pursuit Curves");//set the title of the frame
-		frame.setSize(500, 500);//set the size of the frame
+		frame.setSize(600, 700);//set the size of the frame
 		frame.setLocationRelativeTo(null); // Center the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//makes sure the frame closes properly
 		frame.setVisible(true);//show the frame
@@ -31,18 +31,18 @@ public class AdvancedTriangle extends JFrame {
 
 class NewPanel extends JPanel {
 	double maxX, maxY, minMaxXY, xCenter, yCenter;
-	
+
 	/**
 	 * Initializes all constants
 	 */
 	void initgr() {
-		maxX = getWidth() - 1;//set maxX to the width of the panel - 1
-		maxY = getHeight() - 1;//set maxY to the height of the panel - 1
+		maxX = 200;//set maxX to the width of the panel - 1
+		maxY = 200;//set maxY to the height of the panel - 1
 		minMaxXY = Math.min(maxX, maxY);//sets minMaxXY to the shorter side
 		xCenter = maxX / 2;//xCenter is halfway to maxX
 		yCenter = maxY / 2;//yCenter is halfway to maxY
 	}
-	
+
 	/**
 	 * Rounds from a float to an integer
 	 * @param x
@@ -51,14 +51,14 @@ class NewPanel extends JPanel {
 	int iX(float x) {
 		return Math.round(x);
 	}
-	
+
 	/**
-	 * Rounds from a float to an integer but also returns y from the bottom of the panel
-	 * @param yCenter2
+	 * Rounds from a float to an integer
+	 * @param y
 	 * @return the rounded integer from y
 	 */
-	int iY(double yCenter2) {
-		return (int) (maxY - Math.round(yCenter2));
+	int iY(double y) {
+		return (int) (Math.round(y));
 	}
 
 	@Override
@@ -70,83 +70,92 @@ class NewPanel extends JPanel {
 		float h = side;//sets the height of the largest rectangle to the side length
 		float q = 0.03F;//q affects how much the points change for each iteration
 		float p = 1 - q;//opposite weighting of q
-		//A, B, C, and D are the points of the largest rectangle
-		float xA = (float) (xCenter - sideHalf);//centers the rectangle horizontally
-		float yA = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
-		float xB = (float) (xCenter + sideHalf);//centers the rectangle horizontally
-		float yB = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
-		float xC = (float) (xCenter + sideHalf);//centers the rectangle horizontally
-		float yC = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
-		float xD = (float) (xCenter - sideHalf);//centers the rectangle horizontally
-		float yD = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
 		Image img = null;
 		try {
-		    img = ImageIO.read(new File("dota2_io_sticker.png"));
+			img = ImageIO.read(new File("dota2_io_sticker.png"));
 		} catch (IOException e) {
 			System.exit(0);
 		}
-		img = img.getScaledInstance(100, 100, 0);
-		g.drawImage(img, iX((float) (xCenter - 50)), iY((float)(yCenter + 50)), null);
-		for (int i = 0; i < 60; i++) {
-			g.setColor(new Color((int) (Math.abs(Math.sin((double)i/50) * 100)) + 100, 100, 50));//sets the colors to random colors
-			//connects the points of the rectangle
-			g.drawLine(iX(xA), iY(yA), iX(xB), iY(yB));
-			g.drawLine(iX(xB), iY(yB), iX(xC), iY(yC));
-			g.drawLine(iX(xC), iY(yC), iX(xD), iY(yD));
-			g.drawLine(iX(xD), iY(yD), iX(xA), iY(yA));
-			//rotates and shrinks the rectangle
-			float xA1 = p * xA + q * xB;
-			float yA1 = p * yA + q * yB;
-			float xB1 = p * xB + q * xC;
-			float yB1 = p * yB + q * yC;
-			float xC1 = p * xC + q * xD;
-			float yC1 = p * yC + q * yD;
-			float xD1 = p * xD + q * xA;
-			float yD1 = p * yD + q * yA;
-			xA = xA1;
-			xB = xB1;
-			xC = xC1;
-			xD = xD1;
-			yA = yA1;
-			yB = yB1;
-			yC = yC1;
-			yD = yD1;
+		img = img.getScaledInstance(40, 40, 0);
+		while (yCenter + sideHalf <= getHeight()) {
+			xCenter = maxX / 2;
+			while (xCenter + sideHalf <= getWidth()) {
+				g.drawImage(img, iX((float) (xCenter - 20)), iY((float)(yCenter - 20)), null);
+				//A, B, C, and D are the points of the largest rectangle
+				float xA = (float) (xCenter - sideHalf);//centers the rectangle horizontally
+				float yA = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
+				float xB = (float) (xCenter + sideHalf);//centers the rectangle horizontally
+				float yB = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
+				float xC = (float) (xCenter + sideHalf);//centers the rectangle horizontally
+				float yC = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
+				float xD = (float) (xCenter - sideHalf);//centers the rectangle horizontally
+				float yD = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
+				for (int i = 0; i < 60; i++) {
+					g.setColor(new Color((int) (Math.abs(Math.sin((double)i/50) * 100)) + 100, 100, 50));//sets the colors to random colors
+					//connects the points of the rectangle
+					g.drawLine(iX(xA), iY(yA), iX(xB), iY(yB));
+					g.drawLine(iX(xB), iY(yB), iX(xC), iY(yC));
+					g.drawLine(iX(xC), iY(yC), iX(xD), iY(yD));
+					g.drawLine(iX(xD), iY(yD), iX(xA), iY(yA));
+					//rotates and shrinks the rectangle
+					float xA1 = p * xA + q * xB;
+					float yA1 = p * yA + q * yB;
+					float xB1 = p * xB + q * xC;
+					float yB1 = p * yB + q * yC;
+					float xC1 = p * xC + q * xD;
+					float yC1 = p * yC + q * yD;
+					float xD1 = p * xD + q * xA;
+					float yD1 = p * yD + q * yA;
+					xA = xA1;
+					xB = xB1;
+					xC = xC1;
+					xD = xD1;
+					yA = yA1;
+					yB = yB1;
+					yC = yC1;
+					yD = yD1;
+				}
+				//A, B, C, and D are the points of the largest rectangle
+				xA = (float) (xCenter + sideHalf);//centers the rectangle horizontally
+				yA = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
+				xB = (float) (xCenter - sideHalf);//centers the rectangle horizontally
+				yB = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
+				xC = (float) (xCenter - sideHalf);//centers the rectangle horizontally
+				yC = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
+				xD = (float) (xCenter + sideHalf);//centers the rectangle horizontally
+				yD = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
+				for (int i = 0; i < 60; i++) {
+					g.setColor(new Color(75, (int) (Math.abs(Math.sin((double)i/20) * 20)) + 140, 175));//sets the colors to random colors
+					//connects the points of the rectangle
+					g.drawLine(iX(xA), iY(yA), iX(xB), iY(yB));
+					g.drawLine(iX(xB), iY(yB), iX(xC), iY(yC));
+					g.drawLine(iX(xC), iY(yC), iX(xD), iY(yD));
+					g.drawLine(iX(xD), iY(yD), iX(xA), iY(yA));
+					//rotates and shrinks the rectangle
+					float xA1 = p * xA + q * xB;
+					float yA1 = p * yA + q * yB;
+					float xB1 = p * xB + q * xC;
+					float yB1 = p * yB + q * yC;
+					float xC1 = p * xC + q * xD;
+					float yC1 = p * yC + q * yD;
+					float xD1 = p * xD + q * xA;
+					float yD1 = p * yD + q * yA;
+					xA = xA1;
+					xB = xB1;
+					xC = xC1;
+					xD = xD1;
+					yA = yA1;
+					yB = yB1;
+					yC = yC1;
+					yD = yD1;
+					//System.out.println(xCenter);
+				}
+				//g.setColor(Color.WHITE);
+				//g.drawString("Trippiness Intensifies", iX((float) xCenter), iY(yCenter));
+				xCenter += side;
+			}
+			//System.out.println(yCenter);
+			yCenter += side;
 		}
-		//A, B, C, and D are the points of the largest rectangle
-		xA = (float) (xCenter + sideHalf);//centers the rectangle horizontally
-		yA = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
-		xB = (float) (xCenter - sideHalf);//centers the rectangle horizontally
-		yB = (float) (yCenter - 0.5 * h);//centers the rectangle vertically
-		xC = (float) (xCenter - sideHalf);//centers the rectangle horizontally
-		yC = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
-		xD = (float) (xCenter + sideHalf);//centers the rectangle horizontally
-		yD = (float) (yCenter + 0.5F * h);//centers the rectangle vertically
-		for (int i = 0; i < 60; i++) {
-			g.setColor(new Color(75, (int) (Math.abs(Math.sin((double)i/20) * 20)) + 140, 175));//sets the colors to random colors
-			//connects the points of the rectangle
-			g.drawLine(iX(xA), iY(yA), iX(xB), iY(yB));
-			g.drawLine(iX(xB), iY(yB), iX(xC), iY(yC));
-			g.drawLine(iX(xC), iY(yC), iX(xD), iY(yD));
-			g.drawLine(iX(xD), iY(yD), iX(xA), iY(yA));
-			//rotates and shrinks the rectangle
-			float xA1 = p * xA + q * xB;
-			float yA1 = p * yA + q * yB;
-			float xB1 = p * xB + q * xC;
-			float yB1 = p * yB + q * yC;
-			float xC1 = p * xC + q * xD;
-			float yC1 = p * yC + q * yD;
-			float xD1 = p * xD + q * xA;
-			float yD1 = p * yD + q * yA;
-			xA = xA1;
-			xB = xB1;
-			xC = xC1;
-			xD = xD1;
-			yA = yA1;
-			yB = yB1;
-			yC = yC1;
-			yD = yD1;
-		}
-		//g.setColor(Color.WHITE);
-		//g.drawString("Trippiness Intensifies", iX((float) xCenter), iY(yCenter));
 	}
 }
