89ee1d436e93069b9f576a66113fc614ec0882ed
1j4k7 <jasonkuo147@gmail.com>
2015-10-12 15:19:09 -0400

    created buttonball project

---
+++ ButtonBall/src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		
+	}
+
+}
