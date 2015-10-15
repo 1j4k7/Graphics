ef2e2bd7c594ba25b51c6ba9b4ea83fa746acabe
1j4k7 <jasonkuo147@gmail.com>
2015-10-15 11:58:47 -0400

    buttonball stuff

--- ButtonBall/src/Main.java
+++ ButtonBall/src/Main.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +16,19 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		
+		ImageView image = new ImageView(new Image("dota2_io_sticker.png"));
+		image.setFitHeight(50);
+		image.setFitWidth(50);
+		image.setX(250);
+		image.setY(250);
+		Button left = new Button("Left");
+		Button right = new Button("Right");
+		Button up = new Button("Up");
+		Button down = new Button("Down");
+		Pane pane = new Pane(image, left, right, up, down);
+		stage.setScene(new Scene(pane, 500, 500));
+		stage.setTitle("Tether");
+		stage.show();
 	}
 
 }
