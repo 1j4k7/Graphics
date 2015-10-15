c09cfae6e060be8724b27fc68411a1086566e47a
1j4k7 <jasonkuo147@gmail.com>
2015-10-15 18:17:53 -0400

    Buttonball (tether) complete

--- ButtonBall/src/Main.java
+++ ButtonBall/src/Main.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,22 +14,73 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-
+	
+	private ImageView image = new ImageView(new Image("dota2_io_sticker.png"));
+	private double movement = 50;
+	
 	@Override
 	public void start(Stage stage) {
-		ImageView image = new ImageView(new Image("dota2_io_sticker.png"));
 		image.setFitHeight(100);
 		image.setFitWidth(100);
-		image.setX(250);
-		image.setY(250);
+		image.setX(200);
+		image.setY(200);
 		Button left = new Button("Left");
 		Button right = new Button("Right");
 		Button up = new Button("Up");
 		Button down = new Button("Down");
-		Pane pane = new Pane(image, left, right, up, down);
+		left.setMinWidth(75);
+		right.setMinWidth(75);
+		up.setMinWidth(75);
+		down.setMinWidth(75);
+		left.setOnAction(new LeftEventHandler());
+		right.setOnAction(new RightEventHandler());
+		up.setOnAction(new UpEventHandler());
+		down.setOnAction(new DownEventHandler());
+		HBox buttonRow = new HBox(left, right, up, down);
+		buttonRow.setSpacing(50);
+		buttonRow.setTranslateX(20);
+		BorderPane pane = new BorderPane();
+		pane.getChildren().add(image);
+		pane.setBottom(buttonRow);
 		stage.setScene(new Scene(pane, 500, 500));
 		stage.setTitle("Tether");
 		stage.show();
+	}
+	
+	class LeftEventHandler implements EventHandler<ActionEvent> {
+
+		public void handle(ActionEvent event) {
+			System.out.println("Left");
+			image.setX(image.getX() - 50);
+		}
+		
+	}
+	
+	class RightEventHandler implements EventHandler<ActionEvent> {
+
+		public void handle(ActionEvent event) {
+			System.out.println("Right");
+			image.setX(image.getX() + 50);
+		}
+		
+	}
+	
+	class UpEventHandler implements EventHandler<ActionEvent> {
+
+		public void handle(ActionEvent event) {
+			System.out.println("Up");
+			image.setY(image.getY() - 50);
+		}
+		
+	}
+	
+	class DownEventHandler implements EventHandler<ActionEvent> {
+
+		public void handle(ActionEvent event) {
+			System.out.println("Down");
+			image.setY(image.getY() + 50);
+		}
+		
 	}
 
 }
